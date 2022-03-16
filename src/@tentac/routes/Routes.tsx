e65ef9993fd9326585cfc3bf2265a38cd7f895a9
0x21106.js <aykhansab@code.edu.az>
2022-03-16 17:22:14 +0400

    added home route to routes

--- src/@tentac/routes/Routes.tsx
+++ src/@tentac/routes/Routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthenticationPage } from "../../pages";
+import Home from "../../pages/Home/Home";
 
 export default function GeneralRoutes(props: any) {
   return (
     <BrowserRouter>
       <Routes {...props}>
-        <Route path="/" element={<AuthenticationPage />} />
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/authentication" element={<AuthenticationPage />} />
       </Routes>
     </BrowserRouter>
   );
